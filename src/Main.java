b31782b7bb049d78b0c36ec7e7c4dc31eb6c98df
DanielRygaard <daniel.rygaard@live.se>
2018-08-30 11:02:43 +0200

    Nummersumma
    FörstaUppg

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,38 @@
 
 public class Main {
 
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println(nummersumma(1,4,5));
+	}
+	
+	public static int nummersumma(int... nummer){
+
+		int res = 0;
+		
+		for(int x = 0; x<nummer.length; x++){
+			
+			res +=nummer[x];
+		}
+		
+		
+		
+		return res;
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
